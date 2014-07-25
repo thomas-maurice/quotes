0d73ea2a78d9d82f02ad58fbbc7410d7f73fa241
thomas <tmaurice59@gmail.com>
2014-07-25 07:16:58 +0200

    added tracing to buildstatic

--- buildstatic.sh
+++ buildstatic.sh
@@ -3,12 +3,23 @@
 BWD=bower_components
 SD=static
 
+if [ -z "`which bower`" ]; then
+	echo "You need to install bower ! Type
+	npm install -g bower
+to do so."
+  exit 1
+fi;
+
+echo "Creating directory structure"
+
 if ! [ -d $SD ]; then mkdir $SD; fi;
 if ! [ -d $SD/js ]; then mkdir $SD/js; fi;
 if ! [ -d $SD/css ]; then mkdir $SD/css; fi;
 
+echo "Running bower"
 bower install
 
+echo "Copying files"
 cp $BWD/chartjs/Chart.min.js $SD/js
 cp $BWD/hint.css/hint.min.css $SD/css
 cp $BWD/jquery/dist/jquery.min.js $SD/js
